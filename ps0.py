cc8dfb2d7cae0912c62bf2a2bb9fbab25486040a
Kyle Smith <kj10068@gmail.com>
2020-06-02 21:39:23 -0700

    Add README.md and ps0.py

---
+++ ps0.py
@@ -0,0 +1,13 @@
+import numpy as np
+# Ask user for x
+x = int(input("Enter number x: "))
+
+
+# Ask user for y
+y = int(input("Enter number x: "))
+
+# Print the result of x to the power of y
+print('x**y = ' + str(x**y))
+
+# Prints out the log (base 2) of “x”
+print('log(x) = ' + str(np.log2(x)))
